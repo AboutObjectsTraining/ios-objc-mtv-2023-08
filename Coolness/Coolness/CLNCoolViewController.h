7d19dc69e2ffa1feb58f896e9840d1d23959fab2
Jonathan Lehr <jonathan@aboutobjects.com>
2023-08-17 12:08:27 -0400

    Final version of Coolness

--- Coolness/Coolness/CLNCoolViewController.h
+++ Coolness/Coolness/CLNCoolViewController.h
@@ -6,3 +6,4 @@
 @interface CLNCoolViewController : UIViewController
 
 @end
+
--- Coolness/Coolness/CLNCoolViewController.m
+++ Coolness/Coolness/CLNCoolViewController.m
@@ -4,17 +4,82 @@
 #import "CLNCoolViewController.h"
 #import "CLNCoolViewCell.h"
 
+@interface CLNCoolViewController () <UITextFieldDelegate>
+@property (strong, nonatomic) UIView *contentView;
+@property (strong, nonatomic) UITextField *textField;
+@end
+
 @implementation CLNCoolViewController
+
+/// Responder chain experiement
+/// - Parameters:
+///   - touches: the set of touches
+///   - event: the event
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    NSLog(@"In %s", __func__);
+    // Call to super forwards up the responder chain.
+    [super touchesBegan:touches withEvent:event];
+}
+
+- (void)addCell {
+    NSLog(@"In %s", __func__);
+    CLNCoolViewCell *newCell = [[CLNCoolViewCell alloc] init];
+    newCell.text = self.textField.text;
+    newCell.backgroundColor = UIColor.systemGreenColor;
+    [self.contentView addSubview:newCell];
+}
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField {
+    [self.textField resignFirstResponder];
+    return YES;
+}
 
 - (void)loadView {
     self.view = [[UIView alloc] init];
     self.view.backgroundColor = UIColor.systemBrownColor;
     
+    UIScreen *mainScreen = UIScreen.mainScreen;
+    CGRect accessoryRect;
+    CGRect contentRect;
+    CGRectDivide(mainScreen.bounds, &accessoryRect, &contentRect, 106, CGRectMinYEdge);
+    
+    UIView *accessoryView = [[UIView alloc] initWithFrame:accessoryRect];
+    self.contentView = [[UIView alloc] initWithFrame:contentRect];
+    
+    self.contentView.clipsToBounds = YES;
+    
+    [self.view addSubview:accessoryView];
+    [self.view addSubview:self.contentView];
+    
+    accessoryView.backgroundColor = [UIColor colorWithWhite:1 alpha:0.6];
+    self.contentView.backgroundColor = [UIColor colorWithWhite:1 alpha:0.4];
+    
+    // Controls
+    
+    self.textField = [[UITextField alloc] initWithFrame:CGRectMake(20, 60, 240, 36)];
+    [accessoryView addSubview:self.textField];
+    self.textField.borderStyle = UITextBorderStyleRoundedRect;
+    self.textField.placeholder = @"Type something here...";
+    self.textField.clearButtonMode = UITextFieldViewModeWhileEditing;
+    
+    self.textField.delegate = self;
+    
+    UIButton *button = [UIButton buttonWithType:UIButtonTypeSystem];
+    [accessoryView addSubview:button];
+    
+    [button setTitle:@"Add Cell" forState:UIControlStateNormal];
+    [button sizeToFit];
+    button.frame = CGRectOffset(button.frame, 280, 60);
+    
+    [button addTarget:self action:@selector(addCell) forControlEvents:UIControlEventTouchUpInside];
+    
+    // Cells
+    
     CLNCoolViewCell *subview1 = [[CLNCoolViewCell alloc] initWithFrame:CGRectMake(50, 100, 160, 40)];
     CLNCoolViewCell *subview2 = [[CLNCoolViewCell alloc] initWithFrame:CGRectMake(70, 160, 160, 40)];
     
-    [self.view addSubview:subview1];
-    [self.view addSubview:subview2];
+    [self.contentView addSubview:subview1];
+    [self.contentView addSubview:subview2];
     
     subview1.text = @"Hello World! 🌎🌏🌍";
     subview2.text = @"Cool View Cells Rock! 🍾🥂";
